21a555c8daf53b73f18b93af1a3de532a5e20027
Gerardo Puga <glpuga@ekumenlabs.com>
2022-05-02 22:32:07 -0300

    Update build_team_system

--- project/the_italian_job/tijchallenger/config/submission/build_team_system.bash
+++ project/the_italian_job/tijchallenger/config/submission/build_team_system.bash
@@ -1,113 +1,91 @@
 #!/usr/bin/env bash
 
-#
-# Installation of the ROS system
-#
-
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
 export DIST="${ROS_DISTRO}"
+export RELEASE_TAG="qualifiers_round_release"
+
+# #############################################################################
+#
+# Installation of the ROS system (based on the docker image)
+#
 
 # Base system tools
-apt update \
- && apt install -y \
-    build-essential \
-    cppcheck \
-    curl \
-    cmake \
-    lsb-release \
-    gdb \
-    git \
-    mercurial \
-    python3-dbg \
-    python3-pip \
-    python3-venv \
-    ruby \
-    software-properties-common \
-    sudo \
-    vim \
-    wget \
-    libeigen3-dev \
-    pkg-config \
-    protobuf-compiler \
-    mc \
-    locate \
-    xterm \
-    clang-format \
- && apt clean
+sudo apt update &&
+   sudo apt install -y \
+      build-essential \
+      ccache \
+      clang-format \
+      cmake \
+      cppcheck \
+      curl \
+      git \
+      locate \
+      lsb-release \
+      mc \
+      pkg-config \
+      python3-pip \
+      software-properties-common \
+      sudo \
+      tmux \
+      wget &&
+   sudo apt clean
 
-export DEBIAN_FRONTEND=noninteractive \
- && apt update \
- && apt install -y \
-    tzdata \
- && ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime \
- && dpkg-reconfigure --frontend noninteractive tzdata \
- && apt clean
+export DEBIAN_FRONTEND=noninteractive &&
+   sudo apt update &&
+   sudo apt install -y \
+      tzdata &&
+   ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime &&
+   dpkg-reconfigure --frontend noninteractive tzdata &&
+   sudo apt clean
 
-# install the updated ROS 1 key
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-# install the updated ROS 2 key
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# Base ROS melodic system
+sudo apt update &&
+   sudo apt install -y \
+      python-rosdep \
+      python-catkin-lint \
+      python-catkin-tools \
+      ros-${DIST}-desktop-full &&
+   rosdep init &&
+   sudo apt clean
 
-apt update \
- && apt install -y \
-    python-rosdep \
-    ros-${DIST}-controller-manager \
-    ros-${DIST}-effort-controllers \
-    ros-${DIST}-industrial-core \
-    ros-${DIST}-joint-state-publisher \
-    ros-${DIST}-joint-state-publisher-gui \
-    ros-${DIST}-joint-state-controller \
-    ros-${DIST}-joint-trajectory-action \
-    ros-${DIST}-joint-trajectory-action-tools \
-    ros-${DIST}-joint-trajectory-controller \
-    ros-${DIST}-joy \
-    ros-${DIST}-joy-teleop \
-    ros-${DIST}-key-teleop \
-    ros-${DIST}-moveit-fake-controller-manager \
-    ros-${DIST}-moveit-kinematics \
-    ros-${DIST}-moveit-opw-kinematics-plugin \
-    ros-${DIST}-moveit-planners-ompl \
-    ros-${DIST}-moveit-ros-move-group \
-    ros-${DIST}-moveit-ros-visualization \
-    ros-${DIST}-moveit-setup-assistant \
-    ros-${DIST}-moveit-simple-controller-manager \
-    ros-${DIST}-robot-localization \
-    ros-${DIST}-robot-state-publisher \
-    ros-${DIST}-ros-base \
-    ros-${DIST}-rqt \
-    ros-${DIST}-rqt-common-plugins \
-    ros-${DIST}-rviz \
-    ros-${DIST}-teleop-tools \
-    ros-${DIST}-teleop-twist-keyboard \
-    ros-${DIST}-topic-tools \
-    ros-${DIST}-trac-ik \
-    ros-${DIST}-industrial-core \
-    ros-${DIST}-trac-ik-kinematics-plugin \
-    ros-${DIST}-velodyne-simulator \
-    ros-${DIST}-xacro \
- && rosdep init \
- && apt clean
+#
+# Additional GEAR dependencies obtained running
+# rosdep install -si --reinstall --from-path src/ | cut -f7- -d" " | sort
 
-# Add extra ROS packages here
-apt update \
- && apt install -y \
-    ros-${DIST}-image-proc \
-    ros-${DIST}-vision-opencv \
-    ros-melodic-moveit \
-    ros-melodic-moveit* \
-    ros-melodic-moveit-visual-tools \
- && apt clean
+sudo apt update &&
+   sudo apt install -y \
+      libboost-all-dev \
+      libeigen3-dev \
+      libncurses5-dev \
+      libzmq3-dev \
+      python-catkin-lint \
+      python-rospkg \
+      ros-melodic-effort-controllers \
+      ros-melodic-joint-state-publisher-gui \
+      ros-melodic-moveit-fake-controller-manager \
+      ros-melodic-moveit-kinematics \
+      ros-melodic-moveit-planners-ompl \
+      ros-melodic-moveit-resources-panda-moveit-config \
+      ros-melodic-moveit-ros-move-group \
+      ros-melodic-moveit-ros-visualization \
+      ros-melodic-moveit-setup-assistant \
+      ros-melodic-moveit-visual-tools &&
+   sudo apt clean
 
-# Install extra tools tools
-apt update \
- && apt install -y \
-    tmux \
-    ssh \
- && apt clean
+# Additional ros-control dependencies not installed by the previous stages
+sudo apt update &&
+   sudo apt install -y \
+      "ros-melodic-ros-control*" \
+      "ros-melodic-control*" \
+      "ros-melodic-gazebo-ros-control*" \
+      ros-melodic-moveit-simple-controller-manager \
+      ros-melodic-moveit-commander &&
+   sudo apt clean
 
-rosdep update
+RUN rosdep update
 
+# #############################################################################
 #
 # Installation of the ROS repository
 #
@@ -120,14 +98,15 @@ mkdir -p ~/tij_team_ws/src
 cd ~/tij_team_ws/src
 git clone https://github.com/glpuga/ariac2021ws.git
 cd ariac2021ws
-git checkout final_branch
+git checkout $RELEASE_TAG
 
 # Download the extra code (using submodules for this is a pain)
 mkdir -p ~/tij_team_ws/src/ariac2021ws/external
 cd ~/tij_team_ws/src/ariac2021ws/external
 git clone https://github.com/glpuga/ARIAC.git
 cd ARIAC
-git checkout final_branch
+# Checkout the correct branch
+git checkout $RELEASE_TAG
 
 # Build the competition code
 cd ~/tij_team_ws
