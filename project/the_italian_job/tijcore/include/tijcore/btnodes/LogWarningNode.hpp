56161b5ebc34be328dfbe39b1d84c6c05bd6eab8
Gerardo Puga <glpuga@ekumenlabs.com>
2022-05-19 22:52:20 -0300

    Add simple logger nodes to BT

---
+++ project/the_italian_job/tijcore/include/tijcore/btnodes/LogWarningNode.hpp
@@ -0,0 +1,39 @@
+/* Copyright [2022] <TheItalianJob>
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ * Author: Gerardo Puga */
+
+#pragma once
+
+// standard library
+#include <string>
+
+// external
+#include "behaviortree_cpp_v3/action_node.h"
+
+// tijcore
+#include <tijlogger/logger.hpp>
+
+namespace tijcore
+{
+class LogWarningNode : public BT::SyncActionNode
+{
+public:
+  LogWarningNode(const std::string& name, const BT::NodeConfiguration& config)
+    : SyncActionNode(name, config)
+  {
+  }
+
+  static BT::PortsList providedPorts()
+  {
+    return { BT::InputPort<std::string>("message") };
+  }
+
+  BT::NodeStatus tick() override
+  {
+    auto task_parameters = getInput<std::string>("message").value();
+    WARNING(task_parameters);
+    return BT::NodeStatus::SUCCESS;
+  }
+};
+
+}  // namespace tijcore
